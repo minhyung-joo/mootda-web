c8153b35f03bcddd97261af0ce16c980eb0fd836
minhyung-joo <mjoo@connect.ust.hk>
2021-11-19 00:17:01 +0900

    fix sitemap gen

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -1,28 +1,18 @@
 import { getServerSideSitemap } from 'next-sitemap'
 import { GetServerSideProps } from 'next'
 
+//const HOST = "http://localhost:8000"
+const HOST = "https://api.mootda.com"
+
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // Method to source urls from cms
-  const profile_id_res = await fetch('https//api.mootda.com/v1/user/profile_ids/')
+  const profile_id_res = await fetch(`${HOST}/v1/user/profile_ids/`)
   const profile_ids = (await profile_id_res.json()).profile_ids
 
-  const post_id_res = await fetch('https//api.mootda.com/v1/board/post_ids/')
+  const post_id_res = await fetch(`${HOST}/v1/board/post_ids/`)
   const post_ids = (await post_id_res.json()).post_ids
 
-  const fields = [
-    {
-      loc: 'https://example.com', // Absolute url
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-    {
-      loc: 'https://example.com/dynamic-path-2', // Absolute url
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-  ]
+  const fields = []
 
   profile_ids.forEach((id) => {
       fields.push({
